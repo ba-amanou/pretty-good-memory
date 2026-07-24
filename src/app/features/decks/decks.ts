a3ff2ab86cfd626e926e2296578bd78a4797aab1
ba-amanou <218820324+ba-amanou@users.noreply.github.com>
2026-07-24 17:06:21 +0200

    feat(decks): add create/edit deck modal

--- src/app/features/decks/decks.ts
+++ src/app/features/decks/decks.ts
@@ -1,10 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, viewChild } from '@angular/core';
 import { DecksService } from './decks.service';
 import { RouterLink } from '@angular/router';
+import { DeckForm } from './deck-form/deck-form';
+import { Deck } from '../../core/database/deck.model';
+import { Icon } from '../../shared/icon/icon';
 
 @Component({
   selector: 'app-decks',
-  imports: [RouterLink],
+  imports: [RouterLink, DeckForm, Icon],
   templateUrl: './decks.html',
   styleUrl: './decks.scss',
 })
@@ -12,4 +15,13 @@ export class Decks {
   private decksService = inject(DecksService);
   decks = this.decksService.decks;
 
+  private deckForm = viewChild.required<DeckForm>('deckFormDialog');
+
+  openCreate(): void {
+    this.deckForm().open();
+  }
+
+  openEdit(deck: Deck): void {
+    this.deckForm().open(deck);
+  }
 }
