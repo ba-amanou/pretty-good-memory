fc0bdd78f2b63e0874045db772282c9c68ece061
ba-amanou <218820324+ba-amanou@users.noreply.github.com>
2026-07-28 23:28:45 +0200

    chore(database): expose db in dev mode

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { isDevMode } from '@angular/core';
+import { db, PrettyGoodMemoryDatabase } from './app/core/database/db';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+declare global {
+  interface Window {
+    db?: PrettyGoodMemoryDatabase;
+  }
+}
+
+if (isDevMode()) {
+  window.db = db;
+}
